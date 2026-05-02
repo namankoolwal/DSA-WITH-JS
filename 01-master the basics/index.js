26bab98613b7e2fcdbcdfbe28888633d95c2a59e
namankoolwal <namankoolwal1411@gmail.com>
2026-05-02 14:34:28 +0530

    basics

---
+++ 01-master the basics/index.js
@@ -0,0 +1,20 @@
+// Sum of two numbers
+let a = 5
+let b = 10
+console.log(a + b) // 15
+
+// Concatenation of two strings
+let firstName = "John"
+let lastName = "Doe"
+console.log(firstName + " " + lastName) // John Doe
+
+// Swap two variables
+let num1 = 10;
+let num2 = 20;
+
+[num1, num2] = [num2, num1]
+
+console.log(num1, num2) // 20 10
+
+
+console.log((Math.random() * 9000) + 1000) // Random number between 1000 and 9999
